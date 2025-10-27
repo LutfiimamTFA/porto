a7e6b3639f5f6448b653774041c0aade6294cb85
LutfiimamTFA <154486073+LutfiimamTFA@users.noreply.github.com>
2025-10-27 14:54:57 +0700

    Update Router.jsx

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,15 +1,18 @@
 import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Loading from "../components/common/loading/Loading";
+
 const Home = lazy(() => import("../pages/Home"));
 const Main = lazy(() => import("../layouts/Main"));
 
+// Ambil nama repo dari env, default ""
 const repoName = import.meta.env.VITE_REPO_NAME || "";
 
+// Buat router
 export const router = createBrowserRouter(
   [
     {
-      path: `/`,
+      path: "/", // path utama parent
       element: (
         <Suspense fallback={<Loading />}>
           <Main />
@@ -17,11 +20,18 @@ export const router = createBrowserRouter(
       ),
       children: [
         {
-          path: "/",
-          element: <Home></Home>,
+          index: true, // index route otomatis cocok dengan parent `/`
+          element: <Home />,
         },
+        // Tambahkan rute lain di sini jika perlu
+        // {
+        //   path: "about",
+        //   element: <About />,
+        // },
       ],
     },
   ],
-  { basename: `/${repoName}` }
+  {
+    basename: `/${repoName}`, // basename dinamis, misal "/picto"
+  }
 );
